d52213710f4edbc20994dfe03379d66f3d43a96b
Kim Weincek <kim@wildmountain.agency>
2023-10-21 20:13:18 -0600

    10.21.22 --update partial

--- js/counter.js
+++ js/counter.js
@@ -1,10 +1,37 @@
-const allProjectNums = document.getElementsByClassName('project-num');
+/* INDEX.HTML -- PROJECT COUNTER */
+//Master Key
+let logos = {
+	"favicon": "images/WMA_logos/iconSize/favicon-WMA-logo.jpeg",
+	"header-logo": "images/WMA_logos/iconSize/WMA-medium-logo.png"
+}
+
+let projectNamesOrder = [
+	{"projectSubtitle": "ML Sports Cards Plus", "projectNum": 1},
+	{"projectSubtitle": "Sport Kids Couture", 'projectNum': 2},
+	{"projectSubtitle": "Pexels Photo Search App", 'projectNum': 3},
+	{"projectSubtitle": "Evergreen Brew", 'projectNum': 4},
+	{"projectSubtitle": "Revel Wheel", 'projectNum': 5},
+	{"projectSubtitle": "United Outdoor Store", 'projectNum': 6},
+	{"projectSubtitle": "Max Strong", 'projectNum': 7},
+	{"projectSubtitle": "Fast Food Delivery", 'projectNum': 8},
+	{"projectSubtitle": "Freemote Blog", 'projectNum': 9},
+	{"projectSubtitle": "iCalnex Landing Page", 'projectNum': 10},
+	{"projectSubtitle": "Coming Soon", 'projectNum': 11}
+];
+
+let allProjectSubtitlesDOM = document.getElementsByClassName('project-subtitle');
+let allProjectNumsDOM = document.getElementsByClassName('project-num');
 let count = 0;
-for(let projectNum of allProjectNums){
+
+for(projectSubtitle of allProjectSubtitlesDOM){
+	projectSubtitle.innerHTML = projectNamesOrder[count].projectSubtitle;
 	count++;
-	if(count < 10){
-		projectNum.textContent = `0${count}`;
-	} else if(count >= 10){
-		projectNum.textContent = `${count}`;
+}
+
+for(var i=0; i <= 11; i++){
+	if(i < 9){
+		allProjectNumsDOM[i].innerHTML = `0${projectNamesOrder[i].projectNum}`;
+	} else {
+		allProjectNumsDOM[i].innerHTML = projectNamesOrder[i].projectNum;
 	}
 }
